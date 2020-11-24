b310dde0a99daf06c1ae5057c8bf9337a7d21248
Will Szumski <will@stackhpc.com>
2020-11-24 11:28:48 +0000

    Fix for PBR not supporting 'bare' entry points
    ERROR: For req: slurm-openstack-tools==1.0.1.dev1. Invalid script entry point: <ExportEntry slurm-stats = slurm_openstack_tools.sacct:None []> - A callable suffix is required. Cf https://packaging.python.org/specifications/entry-points/#use-for-scripts for more information.

--- slurm_openstack_tools/sacct.py
+++ slurm_openstack_tools/sacct.py
@@ -7,114 +7,118 @@ import re
 
 from ClusterShell import NodeSet
 
-args = ["sacct", "-X", "--allusers", "--parsable2", "--format",
+SLURM_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+TIMESTAMP_FILE = "lasttimestamp"
+
+def main():
+    args = ["sacct", "-X", "--allusers", "--parsable2", "--format",
         "jobid,jobidraw,cluster,partition,account,group,gid,"
         "user,uid,submit,eligible,start,end,elapsed,elapsedraw,exitcode,state,nnodes,"
         "ncpus,reqcpus,reqmem,reqgres,reqtres,timelimit,nodelist,jobname",
         "--state",
         "CANCELLED,COMPLETED,FAILED,NODE_FAIL,PREEMPTED,TIMEOUT"]
+    # Work out starttime and endtime
+    now = datetime.datetime.utcnow()
+    end_str = now.strftime(SLURM_DATE_FORMAT)
 
-SLURM_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
-TIMESTAMP_FILE = "lasttimestamp"
+    try:
+        with open(TIMESTAMP_FILE) as f:
+            start_str = f.read()
+    except FileNotFoundError:
+        # Default to start of today
+        start_str = "00:00:00"
 
-# Work out starttime and endtime
-now = datetime.datetime.utcnow()
-end_str = now.strftime(SLURM_DATE_FORMAT)
+    args += ["--starttime", start_str]
+    args += ["--endtime", end_str]
 
-try:
-    with open(TIMESTAMP_FILE) as f:
-        start_str = f.read()
-except FileNotFoundError:
-    # Default to start of today
-    start_str = "00:00:00"
+    #print(" ".join(args))
+    process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="UTF-8")
 
-args += ["--starttime", start_str]
-args += ["--endtime", end_str]
+    # Use the title line to work out the attribute order
+    lines = process.stdout.split("\n")
+    titles_line = lines[0]
+    attributes = titles_line.split("|")
 
-#print(" ".join(args))
-process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="UTF-8")
+    # Try to output any errors we might have hit
+    if len(attributes) < 3:
+        print(lines)
+        exit(-1)
 
-# Use the title line to work out the attribute order
-lines = process.stdout.split("\n")
-titles_line = lines[0]
-attributes = titles_line.split("|")
+    # Parse each line of sacct output into a dict
+    items = []
+    for line in lines[1:]:
+      components = line.split("|")
+      if len(components) != len(attributes):
+          continue
 
-# Try to output any errors we might have hit
-if len(attributes) < 3:
-    print(lines)
-    exit(-1)
+      item = {}
+      for i in range(len(attributes)):
+          key = attributes[i]
+          value = components[i]
 
-# Parse each line of sacct output into a dict
-items = []
-for line in lines[1:]:
-  components = line.split("|")
-  if len(components) != len(attributes):
-      continue
+          # Try to convert to int
+          if "JobID" not in key:
+              try:
+                  value = int(value)
+              except:
+                  pass
+          item[key] = value
 
-  item = {}
-  for i in range(len(attributes)):
-      key = attributes[i]
-      value = components[i]
+      # Unpack NodeList format, so its easier to search for hostnames
+      nodelist = item.get("NodeList")
+      if nodelist:
+          nodeset = NodeSet.NodeSet(nodelist)
+          nodes = list([x for x in nodeset])
+          item["AllNodes"] = nodes
+          # Produce a prometheus style regex
+          nodes_regex = "|".join([re.escape(x) for x in nodes])
+          item["AllNodesRegex"] = nodes_regex
 
-      # Try to convert to int
-      if "JobID" not in key:
-          try:
-              value = int(value)
-          except:
-              pass
-      item[key] = value
+      start = item.get("Start")
+      if start:
+          item["StartEpoch"] = int(datetime.datetime.strptime(start, SLURM_DATE_FORMAT).timestamp() * 1000)
 
-  # Unpack NodeList format, so its easier to search for hostnames
-  nodelist = item.get("NodeList")
-  if nodelist:
-      nodeset = NodeSet.NodeSet(nodelist)
-      nodes = list([x for x in nodeset])
-      item["AllNodes"] = nodes
-      # Produce a prometheus style regex
-      nodes_regex = "|".join([re.escape(x) for x in nodes])
-      item["AllNodesRegex"] = nodes_regex
+      end = item.get("End")
+      if end:
+          item["EndEpoch"] = int(datetime.datetime.strptime(end, SLURM_DATE_FORMAT).timestamp() * 1000)
 
-  start = item.get("Start")
-  if start:
-      item["StartEpoch"] = int(datetime.datetime.strptime(start, SLURM_DATE_FORMAT).timestamp() * 1000)
+      # Exclude job steps
+      jobid = item.get("JobID")
+      if jobid and "." not in jobid:
+          items.append(item)
+          print(json.dumps(item))
 
-  end = item.get("End")
-  if end:
-      item["EndEpoch"] = int(datetime.datetime.strptime(end, SLURM_DATE_FORMAT).timestamp() * 1000)
+    # Write out timestamp, so we know where to start next time
+    next = now + datetime.timedelta(seconds=1)
+    next_str = next.strftime(SLURM_DATE_FORMAT)
+    with open(TIMESTAMP_FILE, 'w') as f:
+       f.write(next_str)
 
-  # Exclude job steps
-  jobid = item.get("JobID")
-  if jobid and "." not in jobid:
-      items.append(item)
-      print(json.dumps(item))
+    #print(len(items))
 
-# Write out timestamp, so we know where to start next time
-next = now + datetime.timedelta(seconds=1)
-next_str = next.strftime(SLURM_DATE_FORMAT)
-with open(TIMESTAMP_FILE, 'w') as f:
-   f.write(next_str)
+    # Do a per node summary of job ids
+    # TODO: arguments to toggle this output
+    import collections
+    node_jobs = collections.defaultdict(list)
+    jobs = {}
+    for job in items:
+        jobs[job["JobID"]] = job
+        for node in job["AllNodes"]:
+            node_jobs[node] += [{
+              "id": job["JobID"],
+              "start": job["Start"],
+              "end": job["End"],
+            }]
 
-#print(len(items))
+    #print(jobs)
+    node_info = {
+      "node_info": dict(node_jobs),
+      "start": start_str,
+      "end": end_str,
+    }
+    #if node_info["node_info"]:
+    #    print(node_info)
 
-# Do a per node summary of job ids
-# TODO: arguments to toggle this output
-import collections
-node_jobs = collections.defaultdict(list)
-jobs = {}
-for job in items:
-    jobs[job["JobID"]] = job
-    for node in job["AllNodes"]:
-        node_jobs[node] += [{
-          "id": job["JobID"],
-          "start": job["Start"],
-          "end": job["End"],
-        }]
+if __name__ == "__main__":
+  main()
 
-#print(jobs)
-node_info = {
-  "node_info": dict(node_jobs),
-  "start": start_str,
-  "end": end_str,
-}
-#if node_info["node_info"]:
-#    print(node_info)
