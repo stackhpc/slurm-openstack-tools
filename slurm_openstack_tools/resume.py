cfcd90c6cf6e9a84e2d05e64f62b83d616556496
Will Szumski <will@stackhpc.com>
2021-11-11 12:22:16 +0000

    Make tox happy

--- slurm_openstack_tools/resume.py
+++ slurm_openstack_tools/resume.py
@@ -12,71 +12,93 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-""" A Slurm ResumeProgram to create OpenStack instances.
+"""A Slurm ResumeProgram to create OpenStack instances.
 
-    Usage:
+Usage:
 
-        resume HOSTLIST_EXPRESSION [debug]
-    
-    where:
-        HOSTLIST_EXPRESSION: Name(s) of node(s) to create, using Slurm's hostlist expression, as per [1].
-        debug: Any 2nd argument puts this in debug mode which is more verbose but does not actually create nodes.
+    resume HOSTLIST_EXPRESSION [debug]
 
-    Output and exceptions are written to the syslog. The OpenStack ID(s) of the created node(s) are written to hostname-named files in the Slurm control daemons spool directory [2].
+where: HOSTLIST_EXPRESSION: Name(s) of node(s) to create, using Slurm's
+    hostlist expression, as per [1]. debug: Any 2nd argument puts this in debug
+    mode which is more verbose but does not actually create nodes.
 
-    The flavor, image, network and keypair to be used must be defined as node Features [3] in the format "parameter=value".
+Output and exceptions are written to the syslog. The OpenStack ID(s) of the
+created node(s) are written to hostname-named files in the Slurm control
+daemons spool directory [2].
 
-    OpenStack credentials must be available to this script (e.g. via an application credential in /etc/openstack/clouds.yaml readable by the slurm user).
-    DNS must be available and `SlurmctldParameters=cloud_dns` set in the slurm.conf [4].
+The flavor, image, network and keypair to be used must be defined as node
+Features [3] in the format "parameter=value".
 
-    [1]: https://slurm.schedmd.com/slurm.conf.html#OPT_ResumeProgram
-    [2]: https://slurm.schedmd.com/slurm.conf.html#OPT_SlurmdSpoolDir
-    [3]: https://slurm.schedmd.com/slurm.conf.html#OPT_Features
-    [4]: https://slurm.schedmd.com/slurm.conf.html#OPT_cloud_dns
+OpenStack credentials must be available to this script (e.g. via an
+application credential in /etc/openstack/clouds.yaml readable by the slurm
+user). DNS must be available and `SlurmctldParameters=cloud_dns` set in the
+slurm.conf [4].
+
+[1]: https://slurm.schedmd.com/slurm.conf.html#OPT_ResumeProgram [2]:
+https://slurm.schedmd.com/slurm.conf.html#OPT_SlurmdSpoolDir [3]:
+https://slurm.schedmd.com/slurm.conf.html#OPT_Features [4]:
+https://slurm.schedmd.com/slurm.conf.html#OPT_cloud_dns
 
 """
 
-import sys, os, subprocess, logging.handlers
+import logging.handlers
+import os
+import subprocess
+import sys
+
 import openstack
-import pprint
 
 REQUIRED_PARAMS = ('image', 'flavor', 'keypair', 'network')
 
-# configure logging to syslog - by default only "info" and above categories appear
+# configure logging to syslog - by default only "info" and above
+# categories appear
 logger = logging.getLogger("syslogger")
 logger.setLevel(logging.DEBUG)
 handler = logging.handlers.SysLogHandler("/dev/log")
 handler.setFormatter(logging.Formatter(sys.argv[0] + ': %(message)s'))
 logger.addHandler(handler)
 
+
 def get_statesavelocation():
-    """ Return the path for Slurm's StateSaveLocation """
-    scontrol = subprocess.run(['scontrol', 'show', 'config'], stdout=subprocess.PIPE, universal_newlines=True)
+    """Return the path for Slurm's StateSaveLocation """
+    scontrol = subprocess.run(
+        ['scontrol', 'show', 'config'],
+        stdout=subprocess.PIPE, universal_newlines=True)
     for line in scontrol.stdout.splitlines():
-        if line.startswith('StateSaveLocation'): # StateSaveLocation       = /var/spool/slurm
+        if line.startswith(
+            'StateSaveLocation'):  # StateSaveLocation       = /var/spool/slurm
             return line.split()[-1]
 
+
 def expand_nodes(hostlist_expr):
-    scontrol = subprocess.run(['scontrol', 'show', 'hostnames', hostlist_expr], stdout=subprocess.PIPE, universal_newlines=True)
+    scontrol = subprocess.run(
+        ['scontrol', 'show', 'hostnames', hostlist_expr],
+        stdout=subprocess.PIPE, universal_newlines=True)
     return scontrol.stdout.strip().split('\n')
 
+
 def get_features(nodenames):
-    """ Retrieve the features specified for given node(s).
-    
-        Returns a dict with a key/value pair for each node. Keys are node names, values are lists of strings, one string per feature.
+    """Retrieve the features specified for given node(s).
+
+    Returns a dict with a key/value pair for each node. Keys are node names,
+    values are lists of strings, one string per feature.
     """
-    
-    scontrol = subprocess.run(['scontrol', 'show', 'node', nodenames], stdout=subprocess.PIPE, universal_newlines=True)
+
+    scontrol = subprocess.run(
+        ['scontrol', 'show', 'node', nodenames],
+        stdout=subprocess.PIPE, universal_newlines=True)
     features = {}
     for line in scontrol.stdout.splitlines():
         line = line.strip()
-        if line.startswith('NodeName'): # NodeName=dev-small-cloud-1 CoresPerSocket=1 
+        if line.startswith(
+            'NodeName'):  # NodeName=dev-small-cloud-1 CoresPerSocket=1
             node = line.split()[0].split('=')[1]
         if line.startswith('AvailableFeatures'):
             feature_args = line.split('=', 1)[1]
             features[node] = feature_args.split(',')
-    
+
     return features
+
 
 def create_server(conn, name, image, flavor, network, keypair):
 
@@ -84,9 +106,10 @@ def create_server(conn, name, image, flavor, network, keypair):
         name=name, image_id=image.id, flavor_id=flavor.id,
         networks=[{"uuid": network.id}], key_name=keypair.name,
     )
-    #server = conn.compute.wait_for_server(...)
-    
+    # server = conn.compute.wait_for_server(...)
+
     return server
+
 
 def resume():
     debug = False
@@ -104,17 +127,21 @@ def resume():
     logger.info(f"Read feature information from slurm")
 
     statedir = get_statesavelocation()
-    
+
     for node in new_nodes:
         # extract the openstack parameters from node features:
         if node not in features:
-            logger.error(f"No Feature definitions found for node {node}: {features}")
+            logger.error(
+                f"No Feature definitions found for node {node}: {features}")
         os_parameters = dict(feature.split('=') for feature in features[node])
         if debug:
             logger.info(f"os_parameters for {node}: {os_parameters}")
         missing = set(REQUIRED_PARAMS).difference(os_parameters.keys())
         if missing:
-            logger.error(f"Missing {','.join(missing)} from feature definition for node {node}: {os_parameters}")
+            logger.error(
+                f"Missing {','.join(missing)} from feature definition for "
+                f"node {node}: {os_parameters}"
+            )
 
         # get openstack objects:
         os_objects = {
@@ -125,20 +152,26 @@ def resume():
         }
         not_found = dict((k, v) for (k, v) in os_objects.items() if v is None)
         if not_found:
-            raise ValueError('Could not find openstack objects for: %s' % ', '.join(not_found))
+            raise ValueError(
+                'Could not find openstack objects for: %s' %
+                ', '.join(not_found))
         if debug:
             logger.info(f"os_objects for {node} : {os_objects}")
         if not debug:
             logger.info(f"creating node {node}")
-            server = create_server(conn, node, **os_objects) # TODO: save id to disk so can use it instead of name on deletion (to cope with multiple instances with same name)
+            # TODO(stevebrasier): save id to disk so can use it instead of name
+            # on deletion (to cope with multiple instances with same name)
+            server = create_server(conn, node, **os_objects)
             logger.info(f"server: {server}")
             with open(os.path.join(statedir, node), 'w') as f:
                 f.write(server.id)
-            # Don't need scontrol update nodename={node} nodeaddr={server_ip} as using SlurmctldParameters=cloud_dns
+            # Don't need scontrol update nodename={node} nodeaddr={server_ip}
+            # as using SlurmctldParameters=cloud_dns
+
 
 def main():
     try:
         resume()
-    except:
+    except BaseException:
         logger.exception('Exception in main:')
         raise
